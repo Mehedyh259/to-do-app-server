24c88cd5f99f0f3d33144e8d14ec8daaef3f54df
Mehedyh259 <mehedyh259@gmail.com>
2022-05-19 05:39:36 +0600

    jwt added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const jwt = require('jsonwebtoken')
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,6 +11,22 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+// verify access token {
+const verifyToken = (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    const accessSecret = process.env.TOKEN_SECRET;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'Unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, accessSecret, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'Access Forbidden' });
+        }
+        next();
+    });
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@to-do-cluster.ckukj.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -21,7 +38,15 @@ const run = async () => {
 
         const taskCollection = client.db('todo_app').collection('tasks');
 
-        app.post('/task', async (req, res) => {
+        // genarate access token when login
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.TOKEN_SECRET);
+            res.send({ accessToken });
+        });
+
+
+        app.post('/task', verifyToken, async (req, res) => {
             const task = req.body;
             const result = await taskCollection.insertOne(task);
             if (result) {
@@ -31,13 +56,13 @@ const run = async () => {
             }
         });
 
-        app.get('/task', async (req, res) => {
+        app.get('/task', verifyToken, async (req, res) => {
             const email = req.query.email;
             const filter = { email: email }
             const tasks = await taskCollection.find(filter).toArray();
             res.status(200).send(tasks);
         });
-        app.put('/task/:id', async (req, res) => {
+        app.put('/task/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const options = { upsert: true };
@@ -48,7 +73,7 @@ const run = async () => {
             res.send(result)
         });
 
-        app.delete('/task/:id', async (req, res) => {
+        app.delete('/task/:id', verifyToken, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) }
             const result = await taskCollection.deleteOne(filter);
